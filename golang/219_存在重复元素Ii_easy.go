93fb9562305d1e5a032343d8474c1754bfa86675
adfnekc <i28@foxmail.com>
2020-06-24 18:03:39 +0800

    add 189,217,219

---
+++ golang/219_存在重复元素Ii_easy.go
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=golang
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (39.08%)
+ * Likes:    178
+ * Dislikes: 0
+ * Total Accepted:    48.9K
+ * Total Submissions: 124.7K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的
+ * 绝对值 至多为 k。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ * 
+ * 示例 3:
+ * 
+ * 输入: nums = [1,2,3,1,2,3], k = 2
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+func containsNearbyDuplicate(nums []int, k int) bool {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if v,ok := m[num]; ok && (i - v <= k){
+			return true
+		}
+		m[num] = i
+	}
+	return false
+}
+// @lc code=end
+
